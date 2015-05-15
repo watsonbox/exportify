cda44a4844d90d87e482a527e950d3083a490db5
Howard Wilson <howard@watsonbox.net>
2015-05-15 16:04:58 +0200

    Add basic pagination

--- exportify.js
+++ exportify.js
@@ -1,49 +1,119 @@
+/*
+  TODO:
+
+  - Handle unauthorized response from API
+*/
+
 var PlaylistTable = React.createClass({
+  getInitialState: function() {
+    return {
+      playlists: [],
+      nextURL: null,
+      prevURL: null
+    };
+  },
+
+  loadPlaylists: function(url) {
+    $.ajax({
+      url: url,
+      headers: {
+        'Authorization': 'Bearer ' + this.props.access_token
+      },
+      success: function(response) {
+        console.log(response);
+
+        if (this.isMounted()) {
+          this.setState({
+            playlists: response.items,
+            nextURL: response.next,
+            prevURL: response.previous
+          });
+        }
+      }.bind(this)
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadPlaylists(this.props.url);
+  },
+
   render: function() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th style={{width: "30px"}}></th>
-            <th>Name</th>
-            <th>Owner</th>
-            <th>Tracks</th>
-            <th>Public?</th>
-            <th>Collaborative?</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.playlists.items.map(function(playlist, i) {
-            return (
-              <tr key={i}>
-                <td><i className="fa fa-music"></i></td>
-                <td><a href={playlist.uri}>{playlist.name}</a></td>
-                <td><a href={playlist.owner.uri}>{playlist.owner.id}</a></td>
-                <td>{playlist.tracks.total}</td>
-                <td>{playlist.public ? 'Yes' : 'No'}</td>
-                <td>{playlist.collaborative ? 'Yes' : 'No'}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div>
+        <Paginator nextURL={this.state.nextURL} prevURL={this.state.prevURL} loadPlaylists={this.loadPlaylists}/>
+        <table className="table">
+          <thead>
+            <tr>
+              <th style={{width: "30px"}}></th>
+              <th>Name</th>
+              <th>Owner</th>
+              <th>Tracks</th>
+              <th>Public?</th>
+              <th>Collaborative?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.playlists.map(function(playlist, i) {
+              return <PlaylistRow playlist={playlist} key={playlist.id}/>;
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 });
 
-function startApp(access_token) {
-  $.ajax({
-    url: 'https://api.spotify.com/v1/users/watsonbox/playlists',
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
-    success: function(response) {
-      // Render list of Spotify playlists
-      console.log(response);
-      React.render(<PlaylistTable playlists={response} />, playlists);
+var PlaylistRow = React.createClass({
+  render: function() {
+    playlist = this.props.playlist
+
+    return (
+      <tr key={this.props.key}>
+        <td><i className="fa fa-music"></i></td>
+        <td><a href={playlist.uri}>{playlist.name}</a></td>
+        <td><a href={playlist.owner.uri}>{playlist.owner.id}</a></td>
+        <td>{playlist.tracks.total}</td>
+        <td>{playlist.public ? 'Yes' : 'No'}</td>
+        <td>{playlist.collaborative ? 'Yes' : 'No'}</td>
+      </tr>
+    );
+  }
+});
+
+var Paginator = React.createClass({
+  nextClick: function(e) {
+    e.preventDefault()
+    this.props.loadPlaylists(this.props.nextURL)
+  },
+
+  prevClick: function(e) {
+    e.preventDefault()
+    this.props.loadPlaylists(this.props.prevURL)
+  },
+
+  render: function() {
+    if (this.props.nextURL != null || this.props.prevURL != null) {
+      return (
+        <nav className="text-right">
+          <ul className="pagination pagination-sm">
+            <li className={this.props.prevURL == null ? 'disabled' : ''}>
+              <a href="#" aria-label="Previous" onClick={this.prevClick}>
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li className={this.props.nextURL == null ? 'disabled' : ''}>
+              <a href="#" aria-label="Next" onClick={this.nextClick}>
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )
+    } else {
+      return <div>&nbsp;</div>
     }
-  });
-}
+  }
+});
 
 $(function() {
   var vars = window.location.hash.substring(1).split('&');
@@ -56,6 +126,6 @@ $(function() {
   if (typeof key['access_token'] === 'undefined') {
     $('#loginButton').show()
   } else {
-    startApp(key['access_token']);
+    React.render(<PlaylistTable url="https://api.spotify.com/v1/users/watsonbox/playlists" access_token={key['access_token']} />, playlists);
   }
 });
