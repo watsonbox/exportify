f42f74314880d15bdb373228fed5508cc25be831
Howard Wilson <howard@watsonbox.net>
2020-11-03 16:40:56 +0100

    Revert "Allow starred tracks to be exported [Fixes #5]"
    This reverts commit 687545feb50d1188e3c119fab2381b52bb57b2c1.

--- exportify.js
+++ exportify.js
@@ -55,41 +55,18 @@ var PlaylistTable = React.createClass({
 
   loadPlaylists: function(url) {
     var userId = '';
-    var firstPage = typeof url === 'undefined' || url.indexOf('offset=0') > -1;
 
     window.Helpers.apiCall("https://api.spotify.com/v1/me", this.props.access_token).then(function(response) {
       userId = response.id;
 
-      // Show starred playlist if viewing first page
-      if (firstPage) {
-        return $.when.apply($, [
-          window.Helpers.apiCall(
-            "https://api.spotify.com/v1/users/" + userId + "/starred",
-            this.props.access_token
-          ),
-          window.Helpers.apiCall(
-            "https://api.spotify.com/v1/users/" + userId + "/playlists",
-            this.props.access_token
-          )
-        ])
-      } else {
-        return window.Helpers.apiCall(url, this.props.access_token);
-      }
-    }.bind(this)).done(function() {
-      var response;
-      var playlists = [];
-
-      if (arguments[1] === 'success') {
-        response = arguments[0];
-        playlists = arguments[0].items;
-      } else {
-        response = arguments[1][0];
-        playlists = $.merge([arguments[0][0]], arguments[1][0].items);
-      }
-
+      return window.Helpers.apiCall(
+        typeof url !== 'undefined' ? url : "https://api.spotify.com/v1/users/" + userId + "/playlists",
+        this.props.access_token
+      )
+    }.bind(this)).done(function(response) {
       if (this.isMounted()) {
         this.setState({
-          playlists: playlists,
+          playlists: response.items,
           playlistCount: response.total,
           nextURL: response.next,
           prevURL: response.previous
@@ -154,14 +131,6 @@ var PlaylistRow = React.createClass({
     }
   },
 
-  renderIcon: function(playlist) {
-    if (playlist.name == 'Starred') {
-      return <i className="glyphicon glyphicon-star" style={{ color: 'gold' }}></i>;
-    } else {
-      return <i className="fa fa-music"></i>;
-    }
-  },
-
   render: function() {
     playlist = this.props.playlist
     if(playlist.uri==null) return (
@@ -176,7 +145,7 @@ var PlaylistRow = React.createClass({
     );
     return (
       <tr key={this.props.key}>
-        <td>{this.renderIcon(playlist)}</td>
+        <td><i className="fa fa-music"></i></td>
         <td><a href={playlist.uri}>{playlist.name}</a></td>
         <td><a href={playlist.owner.uri}>{playlist.owner.display_name}</a></td>
         <td>{playlist.tracks.total}</td>
@@ -235,20 +204,11 @@ var PlaylistsExporter = {
     var playlistFileNames = [];
 
     window.Helpers.apiCall("https://api.spotify.com/v1/me", access_token).then(function(response) {
+      var requests = [];
       var limit = 20;
-      var userId = response.id;
 
-      // Initialize requests with starred playlist
-      var requests = [
-        window.Helpers.apiCall(
-          "https://api.spotify.com/v1/users/" + userId + "/starred",
-          access_token
-        )
-      ];
-
-      // Add other playlists
       for (var offset = 0; offset < playlistCount; offset = offset + limit) {
-        var url = "https://api.spotify.com/v1/users/" + userId + "/playlists";
+        var url = "https://api.spotify.com/v1/users/" + response.id + "/playlists";
         requests.push(
           window.Helpers.apiCall(url + '?offset=' + offset + '&limit=' + limit, access_token)
         )
@@ -260,15 +220,7 @@ var PlaylistsExporter = {
 
         // Handle either single or multiple responses
         if (typeof arguments[0].href == 'undefined') {
-          $(arguments).each(function(i, response) {
-            if (typeof response[0].items === 'undefined') {
-              // Single playlist
-              playlists.push(response[0]);
-            } else {
-              // Page of playlists
-              $.merge(playlists, response[0].items);
-            }
-          })
+          $(arguments).each(function(i, response) { $.merge(playlists, response[0].items) })
         } else {
           playlists = arguments[0].items
         }
@@ -309,7 +261,7 @@ var PlaylistExporter = {
 
     for (var offset = 0; offset < playlist.tracks.total; offset = offset + limit) {
       requests.push(
-        window.Helpers.apiCall(playlist.tracks.href.split('?')[0] + '?offset=' + offset + '&limit=' + limit, access_token)
+        window.Helpers.apiCall(playlist.tracks.href + '?offset=' + offset + '&limit=' + limit, access_token)
       )
     }
 
